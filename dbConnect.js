579e0aebbf3c4d4bfc7b2f8d7c4831e9840d2fea
tank@tan-vm2 <xiantank@csie.io>
2020-02-26 15:33:44 +0900

    tmp

--- dbConnect.js
+++ dbConnect.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");;
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(
+	'mongodb+srv://user:user@cluster0-jha0z.mongodb.net/test?retryWrites=true&w=majority',
+	{ autoIndex: false }
+);
+// mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- srv.js
+++ srv.js
@@ -4,19 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
-let privateKey = fs.readFileSync('./cert/key.pem', 'utf8');
-let certificate = fs.readFileSync('./cert/cert.pem', 'utf8');
-let fullchain = fs.readFileSync('./cert/fullchain.pem', 'utf8');
-let chain = fs.readFileSync('./cert/chain.pem', 'utf8');
-let credentials = {
-		key: privateKey,
-		cert: certificate,
-		ca: [
-				fullchain,
-				chain
-		]
-};
+// const https = require('https');
+// let privateKey = fs.readFileSync('./cert/key.pem', 'utf8');
+// let certificate = fs.readFileSync('./cert/cert.pem', 'utf8');
+// let fullchain = fs.readFileSync('./cert/fullchain.pem', 'utf8');
+// let chain = fs.readFileSync('./cert/chain.pem', 'utf8');
+// let credentials = {
+// 		key: privateKey,
+// 		cert: certificate,
+// 		ca: [
+// 				fullchain,
+// 				chain
+// 		]
+// };
 let foodMapRouter = require("./food-map");
 
 let app = express();
@@ -29,6 +29,6 @@ app.use("/food-map", foodMapRouter);
 
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-httpServer.listen(1080);
-httpsServer.listen(1443);
+// var httpsServer = https.createServer(credentials, app);
+httpServer.listen(8080);
+// httpsServer.listen(1443);
